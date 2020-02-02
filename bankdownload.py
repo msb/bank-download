e03190b58cbd84a33880a6734299afd376f69567
Mike Bamford <mike@msb.me.uk>
2020-02-02 11:13:13 +0000

    Implements a script that walks a directory to find and upload CSV files containing bank transactions to a Google spreadsheet.

--- bankdownload.py
+++ bankdownload.py
@@ -17,96 +17,102 @@ SPREADSHEETS_SCOPE = [
     'https://www.googleapis.com/auth/drive',
 ]
 
+# the name of the worksheet maintaining processed files
 WORKSHEET_PROCESSED = 'Processed'
 
+# the name of the transactions worksheet
 WORKSHEET_TRANSACTIONS = 'Transactions'
 
-COLUMNS = [
-    'Account', 'Date', 'Description', 'Type', 'Amount', 'Id', 'Reconciled', 'Category', 'Notes'
-]
-
-ID_INDEX = COLUMNS.index('Id')
-
+# the Google Sheet's epoch date
 EPOCH = datetime.datetime(1899, 12, 30)
 
 
-def create_convert_value(value):
-    def convert_value(_):
-        return value
-    return convert_value
-
-
-def create_convert_identity(index):
-    def convert_identity(row):
-        return row[index]
-    return convert_identity
-
-
 def create_convert_date(index, format):
-    def convert_date(row):
-        return (datetime.datetime.strptime(row[index], format) - EPOCH).days
-    return convert_date
+    """
+    returns a converter for field `index` that converts the value (of format `format`) to a sheet
+    date
+    """
+    return lambda row: (datetime.datetime.strptime(row[index], format) - EPOCH).days
 
 
-def create_convert_amount(index):
+def create_convert_amount_simple(index):
+    """
+    returns a converter for field `index` that converts the value to an amount
+    """
+    return lambda row: abs(float(row[index])) if row[index] else None
+
+
+def create_convert_amount(index, out):
+    """
+    returns a converter for field `index` that converts the value to either an 'in' or 'out amount
+    """
     def convert_amount(row):
-        return float(row[index])
+        amount = float(row[index])
+        amount = abs(min(amount, 0) if out else max(amount, 0))
+        return amount if amount > 0 else None
     return convert_amount
 
 
-def create_convert_amounts(index_in, index_out):
-    def convert_amounts(row):
-        if row[index_in]:
-            return float(row[index_in])
-        return - float(row[index_out])
-    return convert_amounts
+def create_convert_id(indices):
+    """
+    Returns a converter for fields `indices` that converts then to a hash to be used as an id.
+    NOTE: this isn't guaranteed to produce a unique ID also any updates to the transaction data
+    will cause to id to change.
+    """
+    def convert_id(row):
+        sha256 = hashlib.sha256()
+        for i in indices:
+            sha256.update(row[i].encode('utf-8'))
+        return sha256.hexdigest()[:16]
+    return convert_id
 
 
-def monzo_convert_id(row):
-    return row[0]
+# the transactions worksheet's column titles
+COLUMNS = [
+    'Account', 'Date', 'Description', 'Type', 'Money In', 'Money Out', 'Id', 'Reconciled',
+    'Category', 'Notes'
+]
 
+# the indices of key mapped columns
+DATE = COLUMNS.index('Date')
+MONEY_IN = COLUMNS.index('Money In')
+MONEY_OUT = COLUMNS.index('Money Out')
+ID = COLUMNS.index('Id')
 
-def smile_convert_id(row):
-    # FIXME make id smaller
-    # NOTE this isn't guaranteed to produce a unique ID
-    m = hashlib.sha256()
-    for i in range(0, 5):
-        m.update(row[i].encode('utf-8'))
-    return m.hexdigest()
-
-
-def smile_cc_convert_id(row):
-    # NOTE this isn't guaranteed to produce a unique ID
-    m = hashlib.sha256()
-    for i in range(0, 4):
-        m.update(row[i].encode('utf-8'))
-    return m.hexdigest()
-
-
+# A list of converters for a Monzo CSV.
 CONVERSION_MONZO = [
-    (5, monzo_convert_id),
-    (4, create_convert_amount(2)),
-    (1, create_convert_date(1, '%Y-%m-%dT%H:%M:%SZ')),
-    (2, create_convert_identity(8)),
-    (3, create_convert_identity(6)),
-    (6, create_convert_value('x')),
+    create_convert_date(1, '%Y-%m-%dT%H:%M:%SZ'),
+    lambda row: row[8],
+    lambda row: row[6],
+    create_convert_amount(2, False),
+    create_convert_amount(2, True),
+    lambda row: row[0],
+    lambda _: 'x',
 ]
 
+# A list of converters for a Smile CSV.
 CONVERSION_SMILE = [
-        (5, smile_convert_id),
-        (4, create_convert_amounts(3, 4)),
-        (1, create_convert_date(0, '%Y-%m-%d')),
-        (2, create_convert_identity(1)),
-        (3, create_convert_identity(2)),
+    create_convert_date(0, '%Y-%m-%d'),
+    lambda row: row[1],
+    lambda row: row[2],
+    create_convert_amount_simple(3),
+    create_convert_amount_simple(4),
+    create_convert_id(range(0, 5)),
+    lambda _: None,
 ]
 
+# A list of converters for a Smile CC CSV.
 CONVERSION_SMILE_CC = [
-        (5, smile_cc_convert_id),
-        (4, create_convert_amounts(2, 3)),
-        (1, create_convert_date(0, '%Y-%m-%d')),
-        (2, create_convert_identity(1)),
+    create_convert_date(0, '%Y-%m-%d'),
+    lambda row: row[1],
+    lambda _: None,
+    create_convert_amount_simple(2),
+    create_convert_amount_simple(3),
+    create_convert_id(range(0, 4)),
+    lambda _: None,
 ]
 
+# A map of conversions keyed on the account name they apply to
 CONVERSIONS = {
     'Monzo': CONVERSION_MONZO,
     'Monzo Joint': CONVERSION_MONZO,
@@ -116,45 +122,19 @@ CONVERSIONS = {
     'Smile CC Joint': CONVERSION_SMILE_CC,
 }
 
-CONVERT_IDS = {
-    account_name: conversions[0][1] for account_name, conversions in CONVERSIONS.items()
-}
 
-CONVERT_AMOUNTS = {
-    account_name: conversions[1][1] for account_name, conversions in CONVERSIONS.items()
-}
-
-CONVERT_DATES = {
-    account_name: conversions[2][1] for account_name, conversions in CONVERSIONS.items()
-}
-
-
-def create_converters(account_name):
-    converters = [None] * len(COLUMNS)
-    converters[0] = create_convert_value(account_name)
-    for conversion in CONVERSIONS[account_name]:
-        converters[conversion[0]] = conversion[1]
-    return converters
-
-
-def main():
-    """
-    This script ..
-    """
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        os.environ['SERVICE_ACCOUNT_CREDENTIALS_FILE'], SPREADSHEETS_SCOPE
-    )
-
-    gc = gspread.authorize(credentials)
-
-    spreadsheet = gc.open_by_key(os.environ['SPREADSHEET_KEY'])
-
+def get_or_create_processed(spreadsheet):
+    """Gets or creates the 'Processed' worksheet. If creating, writes the header row"""
     try:
         processed = spreadsheet.worksheet(WORKSHEET_PROCESSED)
     except gspread.WorksheetNotFound:
         processed = spreadsheet.add_worksheet(title=WORKSHEET_PROCESSED, rows=1, cols=1)
         processed.update_cell(1, 1, 'Files Processed')
+    return processed
 
+
+def get_or_create_transactions(spreadsheet):
+    """Gets or creates the 'Transactions' worksheet. If creating, writes the header row"""
     try:
         transactions = spreadsheet.worksheet(WORKSHEET_TRANSACTIONS)
     except gspread.WorksheetNotFound:
@@ -165,51 +145,110 @@ def main():
         for i, column in enumerate(COLUMNS):
             cell_list[i].value = column
         transactions.update_cells(cell_list)
+    return transactions
 
-    home_fs = open_fs(os.environ['INPUT_PATH'])
 
-    def process_download(account_name, file, row_count):
-        file_name = f'{step.path}/{file.name}'
-        LOGGER.info(f'Processing {file_name}')
-        try:
-            processed.find(file_name)
-        except gspread.CellNotFound:
-            convert_id = CONVERT_IDS[account_name]
-            convert_amount = CONVERT_AMOUNTS[account_name]
-            existing = {id for id in transactions.col_values(
-                ID_INDEX + 1, value_render_option='FORMULA'
-            )}
-            with home_fs.open(file_name) as csvfile:
-                reader = csv.reader(csvfile)
-                next(reader)  # headers not required
-                converters = create_converters(account_name)
-                rows = [
-                    row for row in reader
-                    if convert_id(row) not in existing and convert_amount(row) != 0
-                ]
-                if len(rows) > 0:
-                    transactions.add_rows(len(rows))
-                    cell_list = transactions.range(
-                        row_count + 1, 1,
-                        row_count + len(rows), len(COLUMNS)
-                    )
-                    for i, row in enumerate(rows):
-                        for j, convert in enumerate(converters):
-                            if convert:
-                                cell_list[i * len(COLUMNS) + j].value = convert(row)
-                    transactions.update_cells(cell_list)
-                    row_count += len(rows)
-            # FIXME processed.append_row([file_name], value_input_option='RAW')
-        return row_count
+def main():
+    """
+    This script walks a directory to find and upload CSV files containing bank transactions to a
+    Google spreadsheet. The script expects the following directory structure:
+        root
+            {an account name}
+                {any name}.csv
+                {other name}.csv
+                    :
+            {other account name}
+                {any name}.csv
+                {other name}.csv
+                    :
+                :
+    The names of the previously uploaded files are maintained in a worksheet called 'Processed' and
+    not uploaded again. The script also checks that previously uploaded transactions are not
+    uploaded again using the transaction's id. If the transaction doesn't have an id then one is
+    generated. A cut-off date can be set before which no transactions are uploaded (useful when
+    archiving transactions).
+    """
 
-    # row_count doesn't appear to update so we maintain it locally
-    row_count = transactions.row_count
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        os.environ['SERVICE_ACCOUNT_CREDENTIALS_FILE'], SPREADSHEETS_SCOPE
+    )
 
-    for step in home_fs.walk(filter=['*.csv']):
+    gc = gspread.authorize(credentials)
+
+    spreadsheet = gc.open_by_key(os.environ['SPREADSHEET_KEY'])
+
+    # the 'Processed' worksheet
+    processed = get_or_create_processed(spreadsheet)
+
+    # the 'Transactions' worksheet
+    transactions = get_or_create_transactions(spreadsheet)
+
+    # open the input path
+    input_path = open_fs(os.environ['INPUT_PATH'])
+
+    # the date before which transactions should be ignored
+    cut_off_date = os.environ.get('CUT_OFF_DATE', 0)
+
+    # retrieve all the existing ids from the sheet
+    existing_ids = {id for id in transactions.col_values(
+        ID + 1, value_render_option='FORMULA'
+    )}
+
+    def process_download(account_name, file_name):
+        """
+        A closure that processes a CSV `file_name` for a particular `account_name`
+        and returns a list of converted rows.
+        """
+        # get the id, money, and date converters for the account_name
+        convert_id = CONVERSIONS[account_name][ID - 1]
+        convert_money_in = CONVERSIONS[account_name][MONEY_IN - 1]
+        convert_money_out = CONVERSIONS[account_name][MONEY_OUT - 1]
+        convert_date = CONVERSIONS[account_name][DATE - 1]
+        # get the row converters
+        converters = [lambda _: account_name] + CONVERSIONS[account_name]
+        with input_path.open(file_name) as csvfile:
+            reader = csv.reader(csvfile)
+            # discard the header
+            next(reader)
+            # filter all the new non-zero transactions > the cut_off_date
+            # and convert them to sheet rows
+            rows = [
+                [convert(row) for convert in converters]
+                for row in reader if
+                convert_id(row) not in existing_ids and
+                convert_date(row) >= cut_off_date and
+                not (convert_money_in(row) is None and convert_money_out(row) is None)
+            ]
+            return rows
+
+    # a list of converted sheet rows returned from `process_download()`
+    rows = []
+
+    # walk the tree of CSV files
+    for step in input_path.walk(filter=['*.csv']):
         account_name = step.path[1:]
         for file in step.files:
-            if account_name in ('Monzo', 'Smile Joint'):  # FIXME
-                row_count = process_download(account_name, file, row_count)
+            file_name = f'{step.path}/{file.name}'
+            try:
+                processed.find(file_name)
+            except gspread.CellNotFound:
+                # process the file if it doesn't exists in 'Processed'
+                LOGGER.info(f'Processing {file_name}')
+                rows += process_download(account_name, file_name)
+                # add the file_name to processed
+                processed.append_row([file_name], value_input_option='RAW')
+
+    if len(rows) > 0:
+        # add and retrieve the new blank rows
+        transactions.add_rows(len(rows))
+        cell_list = transactions.range(
+            transactions.row_count + 1, 1, transactions.row_count + len(rows), len(COLUMNS)
+        )
+        # populate and update the new rows
+        for i, row in enumerate(rows):
+            for j, value in enumerate(row):
+                cell_list[i * len(COLUMNS) + j].value = value
+        transactions.update_cells(cell_list)
 
 
 if __name__ == "__main__":
